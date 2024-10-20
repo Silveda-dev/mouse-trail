2af0f3c7036775b9e72f48e7682f264251134136
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2024-10-20 21:36:19 +1030

    Add fall and delete mechanism

--- pride-mouse.js
+++ pride-mouse.js
@@ -1,4 +1,5 @@
 let flagArray = [];
+let flagCoords = [];
 
 //Return mouse position when move
 const move_handler = function(e) {
@@ -27,6 +28,24 @@ const new_flag = function(x, y) {
     //Adding flag
     document.body.appendChild(newFlag);
     flagArray.push(newFlag);
+    flagCoords.push(y);
+}
+
+const update_flags = function() {
+    for (let i = 0; i < flagArray.length; i++) {
+        //Make flags fall
+        flagCoords[i] += 10;
+        if (flagCoords[i] > 550) {
+            //Clear flags that have fallen off-screen
+            flagCoords.splice(i,1);
+            document.body.removeChild(flagArray[i]);
+            flagArray.splice(i,1);
+        } else {
+            flagArray[i].style.top = flagCoords[i].toString().concat("px");
+        }
+    }
 }
 
 document.addEventListener("mousemove", move_handler);
+
+setInterval(update_flags, 30);
